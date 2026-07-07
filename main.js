b0de92651a6a63bc2b62e4db5f906ffd79d98ed7
patysevidanis <patricia.sevidanis@Escola.pr.gov.br>
2026-07-07 17:17:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha =12;
+let tamanhoSenha =5;
 numeroSenha .textContent= tamanhoSenha;
 
   const botoes = document.querySelectorAll ('.parametro-senha__botao');
 
   botoes [0].onclick = diminuiTamanho;
+  botoes[1].onclick = aumentaTamanho;
 
-  function diminuiTamanho () {
-  tamanhoSenha --;
-  numeroSenha .textContent= tamanhoSenha;
+  function diminuiTamanho(){
+  if (tamanhoSenha > 1){
+      tamanhoSenha--;
   }
+  numeroSenha .textContent= tamanhoSenha;
+  geraSenha();
+  }
+  function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+  }
+
+  const campoSenha = document.querySelector ('#campo-senha');
+
+  const letrasMaisculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+  geraSenha();
+
+
+  function geraSenha() {
+    let senha='';
+    for(let i=0; i< tamanhoSenha;i++) {
+    let numeroAleatorio= Math.random ()*letrasMaisculas.length;
+  numeroAleatorio= Math.floor (numeroAleatorio);
+  senha = senha + letrasMaisculas [numeroAleatorio];
+  }
+  campoSenha.value = senha;
+}
